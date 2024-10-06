57c40ecec7163fd7b20639f66f2a5b48774c0d17
BloodyNoodles <querubinkingace@gmail.com>
2024-10-06 15:45:29 +0800

    Add product function (not finished)

--- app/src/main/java/com/example/poscanteen/home.java
+++ app/src/main/java/com/example/poscanteen/home.java
@@ -2,6 +2,8 @@ package com.example.poscanteen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -20,6 +22,17 @@ public class home extends AppCompatActivity {
 
         AppCompatButton checkout = findViewById(R.id.checkoutBtn);
         ImageButton menuBtn = findViewById(R.id.menubtn);
+        Button buttonNavigate = findViewById(R.id.buttonNavigate);
+
+
+        // Set up the button to navigate to SecondActivity
+        buttonNavigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home.this, CustomDialogue.class);
+                startActivity(intent);
+            }
+        });
 
         // Add the fragment to the activity
         if (savedInstanceState == null) {
@@ -27,8 +40,6 @@ public class home extends AppCompatActivity {
                     .replace(R.id.sideMenus, new SideMenuFragment())
                     .commit();
         }
-
-
 
         // Checkout button listener
         checkout.setOnClickListener(v -> {
@@ -43,5 +54,7 @@ public class home extends AppCompatActivity {
                 fragment.toggleSideMenu();
             }
         });
+
+
     }
 }
